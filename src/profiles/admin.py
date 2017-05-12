3cdf89175d3fcd2c243b50e2ce245e9b70ad51c0
ravi24748 <lionravi10@gmail.com>
2017-05-12 17:01:35 +0530

    initialialize bolierplate

---
+++ src/profiles/admin.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+from .models import profile
+
+
+# Register your models here.
+class profileAdmin(admin.ModelAdmin):
+	class meta:
+		model = profile
+
+admin.site.register(profile,profileAdmin)		
+
+		
